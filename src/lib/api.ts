0932116f736836616e0ed1eb310b557a8336dd8a
Firaol <loarifse16@gmail.comgit config --global user.email loarifse16@gmail.com>
2025-07-18 00:42:23 +0300

    updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
     ? '/api' 
-    : "http://localhost:4000/api", 
+    : "https://pinggo-t3jz.onrender.com/api", 
   withCredentials: true,                
 });
